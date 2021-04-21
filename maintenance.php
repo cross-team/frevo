a1156e75864199a02ba8879ee2743efd1a91cf2c
Emanuel Costa <emanweb@gmail.com>
2021-04-21 13:13:04 -0700

    Update copy according to Allana's request

--- maintenance.php
+++ maintenance.php
@@ -8,7 +8,7 @@ header("Retry-After: 3600");
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>SafraPay is Under Maintenance</title>
+  <title>We’re temporarily unavailable.</title>
   <style>
   body {font-family: "Source Sans Pro", sans-serif; font-size: 18px;}
   img.maintenance-logo { width: 120px; vertical-align: middle; display: block;}
@@ -22,9 +22,9 @@ header("Retry-After: 3600");
   <p>
   <a href="/"><img src="/wp-content/uploads/2021/02/brand-1.svg" class="maintenance-logo" alt="safrapay logo" loading="lazy"></a>
   </p>
-  <h1>Our site is temporarily unavailable.</h1>
-  <p>Scheduled maintenance is currently in progress. Please check back soon.</p>
-  <p>We apologize for any inconvenience.</p>
+  <h1>We’re temporarily unavailable.</h1>
+    <p>We’ve scheduled a little self-care time to freshen our pixels and tidy our code. We should be ready to greet the world again soon.</p>
+    <p>We apologize for any inconvenience. If you need any assistance in the mean time, don’t hesitate to reach out to our lovely Customer Support Team at 1-877-472-3727.</p>
   </div>
 </article>
 <footer>
